c335317dde991f36238051224cdbd2ddaef8640a
Slim <slim.spade21@gmail.com>
2021-03-18 18:13:30 +0800

    :white_check_mark: Add validation to models

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,8 @@ class LineItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :order
 
+  validates :quantity, presence: true, numericality: { only_integer: true }
+
   def total_price
     quantity * product.price
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :duration_in_days, presence: true, numericality: { only_integer: true }
 end
