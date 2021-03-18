358b1e4db1131e3cc7e7d3bde2a6fb45f93d4f56
Slim <slim.spade21@gmail.com>
2021-03-18 18:15:04 +0800

    :potable_water: Lint

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,6 @@ class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
 
-  # LOGIC
   def sub_total
     sum = 0
     line_items.each do |line_item|
