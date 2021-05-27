350f1376c36f5811c7d3cc37761330ec45ad7c5a
Slim <slim.spade21@gmail.com>
2021-05-27 19:14:18 +0800

    :wrench: Initialize Rails.configuration.stripe wh 'STRIPE_WEBHOOK_SIGNING_SECRET'

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,7 +2,8 @@
 
 Rails.configuration.stripe = {
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key: ENV['STRIPE_SECRET_KEY']
+  secret_key: ENV['STRIPE_SECRET_KEY'],
+  wh: ENV['STRIPE_WEBHOOK_SIGNING_SECRET']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
