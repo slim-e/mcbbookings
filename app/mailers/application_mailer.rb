7fab4ad096f57f221763a4b989174cfea99a1a8d
Slim <slim.spade21@gmail.com>
2021-06-13 14:54:32 +0800

    :bug: Fix email sender name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "resa@mmacampbrazil.com"
+  default from: "Thibault Marino <resa@mmacampbrazil.com>"
   layout "mailer"
 end
