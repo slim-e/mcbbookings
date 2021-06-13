dc4b0a9c12b92a1ae1807d273e4517e3aff26a4e
Slim <slim.spade21@gmail.com>
2021-06-13 12:05:32 +0800

    :bug: Fix misspell in #send_contract

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -11,9 +11,9 @@ class CustomerMailer < ApplicationMailer
 
     camp_durations = @order.products.pluck(:duration_in_days)
     @contract_url =
-      if camp_durations.includes?(92)
+      if camp_durations.include?(92)
         "https://drive.google.com/file/d/1Z0IkUAvUqBFoq-1yIY2xhSSS38TPf7zP/view?usp=sharing"
-      elsif camp_durations.includes?(31)
+      elsif camp_durations.include?(31)
         "https://drive.google.com/file/d/15ndh4eVY0B4JJtpyAcQGrr98niqcJ5uo/view?usp=sharing"
       else
         "https://drive.google.com/file/d/1szHSYIm0mm0xwn2JkI5ytLJQuP0LnYHd/view?usp=sharing"
